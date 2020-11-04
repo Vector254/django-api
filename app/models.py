9d7317e3607c66a334779dd7806efc8f334c84d9
Vector254 <ochrist7@gmail.com>
2020-11-04 08:15:21 +0300

    create project model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=155)
+    link = models.URLField(max_length=255)
+    description = models.TextField(max_length=255)
+   # image = models.ImageField(upload_to = 'images/')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null='True', blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.title}'
